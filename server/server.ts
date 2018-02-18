7c688ce4da15e21df2f57027d25d48968cd294f7
architMahto <architmahto@cmail.carleton.ca>
2018-02-18 18:33:00 -0500

    Established connection to PostgreSQL database.

--- server/server.ts
+++ server/server.ts
@@ -1,1 +1,22 @@
-console.log('Server is running...');
+const ORM = require('sequelize');
+import { Sequelize } from 'Sequelize';
+
+const dbURL = 'postgres://localhost:5432/complete-typescript-course';
+
+const sequelize: Sequelize  = new ORM(dbURL);
+
+const CourseModel = sequelize.define('Course', {
+  description: ORM.STRING,
+  url: ORM.STRING,
+  longDescription: ORM.TEXT,
+  iconUrl: ORM.STRING,
+  courseListIcon: ORM.STRING,
+  seqNo: ORM.INTEGER,
+  comingSoon: ORM.BOOLEAN,
+  isNew: ORM.BOOLEAN,
+  isOngoing: ORM.BOOLEAN
+});
+
+CourseModel.findAll();
+
+console.log(CourseModel.findAll());
